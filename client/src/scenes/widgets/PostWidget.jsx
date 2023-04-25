e58786e6ced5a31a76992df97d0fd9c4f7af8e51
Khushipawar <khushipawar636@gmail.com>
2023-04-25 19:45:42 +0530

    Comment Section for Client Added  Khushi

--- client/src/scenes/widgets/PostWidget.jsx
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -4,9 +4,11 @@ import {
   FavoriteOutlined,
   ShareOutlined,
 } from "@mui/icons-material";
-import { Box, Divider, IconButton, Typography, useTheme } from "@mui/material";
+import { Box, Divider, IconButton, Typography, useTheme,InputBase,Button } from "@mui/material";
+import Comment from "components/Comment";
 import FlexBetween from "components/FlexBetween";
 import Friend from "components/Friend";
+// import UserImage from "components/UserImage";
 import WidgetWrapper from "components/WidgetWrapper";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -24,9 +26,11 @@ const PostWidget = ({
   comments,
 }) => {
   const [isComments, setIsComments] = useState(false);
+  const [comment , setComment] = useState("");
   const dispatch = useDispatch();
   const token = useSelector((state) => state.token);
   const loggedInUserId = useSelector((state) => state.user._id);
+  // const picturePath = useSelector((state)=> state.user.picturePath);
   const isLiked = Boolean(likes[loggedInUserId]);
   const likeCount = Object.keys(likes).length;
 
@@ -35,7 +39,7 @@ const PostWidget = ({
   const primary = palette.primary.main;
 
   const patchLike = async () => {
-    const response = await fetch(`http://localhost:6001/posts/${postId}/like`, {
+    const response = await (`http://localhost:6001/posts/${postId}/like`, {
       method: "PATCH",
       headers: {
         Authorization: `Bearer ${token}`,
@@ -45,6 +49,20 @@ const PostWidget = ({
     });
     const updatedPost = await response.json();
     dispatch(setPost({ post: updatedPost }));
+  };
+
+  const handleComment = async() => {
+    const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/posts/${postId}/${loggedInUserId}/comment`,{
+      method:"POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ comment: comment }), 
+    });
+    const updatedPost = await response.json();
+    dispatch(setPost({ post: updatedPost }));
+    setComment("");
   };
 
   return (
@@ -64,7 +82,7 @@ const PostWidget = ({
           height="auto"
           alt="post"
           style={{ borderRadius: "0.75rem", marginTop: "0.75rem" }}
-          src={`http://localhost:6001/assets/${picturePath}`}
+          src={`${picturePath}`}
         />
       )}
       <FlexBetween mt="0.25rem">
@@ -94,15 +112,46 @@ const PostWidget = ({
       </FlexBetween>
       {isComments && (
         <Box mt="0.5rem">
-          {comments.map((comment, i) => (
+          {comments.slice(0).reverse().map((comment, i) => (
             <Box key={`${name}-${i}`}>
               <Divider />
-              <Typography sx={{ color: main, m: "0.5rem 0", pl: "1rem" }}>
-                {comment}
-              </Typography>
+              <Comment userId={comment.userId} comment={comment.comment} postId={postId}/>
             </Box>
           ))}
           <Divider />
+          <FlexBetween>
+          <InputBase
+          placeholder="Write a comment ..."
+          onChange={(e)=>setComment(e.target.value)}
+          value={comment}           
+          sx={{
+            width: "100%",
+            backgroundColor: palette.neutral.light,
+            borderRadius: "2rem",
+            padding: "1rem 2rem",
+            mt:"1rem"
+          }}
+        />
+        <Button
+          disabled={!comment}
+          onClick={handleComment}
+          sx={{
+            color: palette.background.alt,
+            mt:"1rem",
+            ml:"0.5rem",
+            backgroundColor: palette.primary.main,
+            borderRadius: "3rem",
+            "&:hover":{
+              cursor:"pointer",
+              color: palette.background.alt,
+              backgroundColor: palette.primary.main,
+            }
+          }}
+        >
+          POST
+        </Button>
+          </FlexBetween>
+          
         </Box>
       )}
     </WidgetWrapper>
